e2c39771eacc5c3f69755aaa311317a0b6617f1b
kheina <vixen@kheina.com>
2020-08-06 17:42:49 -0400

    add key_id to tokens

--- authenticator.py
+++ authenticator.py
@@ -1,5 +1,5 @@
 from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey, Ed25519PublicKey
-from kh_common.http_error import Unauthorized, BadRequest, InternalServerError
+from kh_common.http_error import Unauthorized, BadRequest, InternalServerError, NotFound
 from cryptography.hazmat.backends import default_backend as crypto_backend
 from psycopg2.errors import UniqueViolation, ConnectionException
 from secrets import token_bytes, randbelow, compare_digest
@@ -10,16 +10,37 @@ from psycopg2 import Binary, connect as dbConnect
 from argon2 import PasswordHasher as Argon2
 from traceback import format_tb
 from hashlib import sha3_512
+from math import floor, ceil
 from uuid import uuid4
-from math import floor
 from time import time
 import ujson as json
 import sys
 
 
+"""
+table definition:
+CREATE TABLE kheina.auth.token_keys (
+	key_id INT UNIQUE GENERATED ALWAYS AS IDENTITY,
+	algorithm TEXT NOT NULL,
+	public_key BYTEA NOT NULL,
+	private_key BYTEA NOT NULL,
+	secret SMALLINT NOT NULL,
+	issued TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	expires TIMESTAMPTZ NOT NULL DEFAULT NOW() + interval '30 days',
+	PRIMARY KEY (algorithm, key_id)
+);
+CREATE INDEX token_keys_algorithm_issued_expires_joint_index ON kheina.auth.token_keys (algorithm, issued, expires);
+"""
+
+
 def verifyToken(token) :
 	load, signature = tuple(map(b64decode, token.split('.')))
-	version, algorithm, expires, guid, data = load.split(b'.', 4)
+	version, algorithm, key_id, expires, guid, data = load.split(b'.', 4)
+	version = version.decode()
+	algorithm = algorithm.decode()
+	key_id = int.from_bytes(b64decode(key_id), 'big')
+	expires = int.from_bytes(b64decode(expires), 'big')
+	guid = b64decode(guid).hex()
 
 	# fetchPublicKey = lambda expires : a.fetchPublicKey(expires).get('public_key')
 	public_key = Ed25519PublicKey.from_public_bytes(
@@ -38,9 +59,15 @@ class Authenticator :
 		self._initArgon2()
 		self._key_refresh_interval = 60 * 60 * 24  # 24 hours
 		self._token_expires_interval = 60 * 60 * 24 * 30  # 30 days
-		self._private_keys = { }
 		self._token_version = '1'
 		self._token_algorithm = 'ed25519'
+		self._public_keyring = { }
+		self._active_private_key = {
+			'key': None,
+			'algorithm': None,
+			'start': 0,
+			'end': 0,
+		}
 
 
 	def _connect(self) :
@@ -63,22 +90,27 @@ class Authenticator :
 			self._secrets = [bytes.fromhex(salt) for salt in credentials['salts']]
 
 
-	def _query(self, sql, params=(), commit=False, fetch=False, maxretry=2) :
+	def _query(self, sql, params=(), commit=False, fetch_one=False, fetch_all=False, maxretry=2) :
 		try :
 			cur = self._conn.cursor()
 			cur.execute(sql, params)
+
 			if commit :
 				self._conn.commit()
 			else :
 				self._conn.rollback()
-			return cur.fetchall() if fetch else None
+
+			if fetch_one :
+				return cur.fetchone()
+			elif fetch_all :
+				return cur.fetchall()
 
 		except ConnectionException :
 			self._connect()
 			if maxretry > 1 :
 				e, exc_tb = sys.exc_info()[1:]
 				self.logger.warning({ 'message': f'{getFullyQualifiedClassName(e)}: {e}', 'stacktrace': format_tb(exc_tb) })
-				return self._query(sql, params, commit, fetch, maxretry - 1)
+				return self._query(sql, params, commit, fetch_one, fetch_all, maxretry - 1)
 			else :
 				self.logger.exception({ })
 				raise
@@ -99,64 +131,91 @@ class Authenticator :
 		return sha3_512(email.encode() + self._secrets[0]).digest()
 
 
-	def _calc_expires(self, timestamp) :
-		return self._key_refresh_interval * round(timestamp / self._key_refresh_interval)
+	def _calc_timestamp(self, timestamp) :
+		return self._key_refresh_interval * floor(timestamp / self._key_refresh_interval)
 
 
 	def _generate_token(self, token_data: dict) :
-		expires = self._calc_expires(time()) + self._token_expires_interval
+		issued = time()
+		expires = issued + self._token_expires_interval
 
-		if expires in self._private_keys :
-			private_key = self._private_keys[expires]
+		if self._active_private_key['start'] <= issued < self._active_private_key['end'] :
+			private_key = self._active_private_key['key']
 
 		else :
-			# look for an existing public/private key in the db
+			# initialize a new private key
+			start = self._calc_timestamp(issued)
+			end = start + self._key_refresh_interval
+			self._active_private_key = {
+				'key': None,
+				'algorithm': self._token_algorithm,
+				'start': start,
+				'end': end,
+			}
+
+			# look for an existing private key in the db
 			data = self._query("""
-				SELECT private_key, secret
-				FROM kheina.auth.token_key
-				WHERE algorithm = %s AND expires = to_timestamp(%s);
+				SELECT private_key, secret, key_id
+				FROM kheina.auth.token_keys
+				WHERE
+					algorithm = %s
+					AND issued BETWEEN current_date
+						AND current_date + interval '1 day';
 				""",
-				(self._token_algorithm, expires),
-				fetch=True,
+				(self._token_algorithm,),
+				fetch_one=True,
 			)
 
 			if data :
-				pk_load = data[0][0]
-				secret = data[0][1]
-				private_key = self._private_keys[expires] = serialization.load_der_private_key(pk_load, self._secrets[secret], crypto_backend())
+				pk_load = data[0]
+				secret = data[1]
+				key_id = data[2]
+
+				private_key = self._active_private_key['key'] = serialization.load_der_private_key(pk_load, self._secrets[secret], crypto_backend())
+				public_key = private_key.public_key().public_bytes(
+					encoding=serialization.Encoding.Raw,
+					format=serialization.PublicFormat.Raw,
+				)
 				del data, pk_load, secret
 
 			else :
 				secret = randbelow(len(self._secrets))
-				private_key = self._private_keys[expires] = Ed25519PrivateKey.generate()
+				private_key = self._active_private_key['key'] = Ed25519PrivateKey.generate()
+				public_key = private_key.public_key().public_bytes(
+					encoding=serialization.Encoding.Raw,
+					format=serialization.PublicFormat.Raw,
+				)
 
 				# insert the new key into db
 				self._query("""
-					INSERT INTO kheina.auth.token_key
-					(algorithm, public_key, private_key, secret, expires)
+					INSERT INTO kheina.auth.token_keys
+					(public_key, private_key, secret, algorithm, issued, expires)
 					VALUES
-					(%s, %s, %s, %s, to_timestamp(%s));
+					(%s, %s, %s, %s, to_timestamp(%s), to_timestamp(%s))
+					RETURNING key_id;
 					""",
 					(
-						self._token_algorithm,
-						private_key.public_key().public_bytes(
-							encoding=serialization.Encoding.Raw,
-							format=serialization.PublicFormat.Raw
-						),
+						public_key,
 						private_key.private_bytes(
 							encoding=serialization.Encoding.DER,
 							format=serialization.PrivateFormat.PKCS8,
-							encryption_algorithm=serialization.BestAvailableEncryption(self._secrets[secret])
+							encryption_algorithm=serialization.BestAvailableEncryption(self._secrets[secret]),
 						),
 						secret,
-						expires,
+						self._token_algorithm,
 					),
 					commit=True,
+					fetch_one=True,
 				)
+				key_id = data[0]
+
+			# put the new key into the public keyring
+			self._public_keyring[(self._token_algorithm, key_id)] = b64encode(public_key).decode()
 
 		load = b'.'.join([
 			self._token_version.encode(),
 			self._token_algorithm.encode(),
+			b64encode(key_id.to_bytes(ceil(key_id.bit_length() / 8), 'big')),
 			b64encode(expires.to_bytes(ceil(expires.bit_length() / 8), 'big')),
 			b64encode(uuid4().bytes),
 			json.dumps(token_data).encode(),
@@ -171,42 +230,36 @@ class Authenticator :
 		}
 
 	
-	def fetchPublicKey(self, expires, algorithm=None) :
-		if expires < time() :
-			raise Unauthorized('Token has expired.')
-
+	def fetchPublicKey(self, key_id, algorithm=None) :
 		if not algorithm :
 			algorithm = self._token_algorithm
 
-		expires = self._calc_expires(expires)
-		public_key = None
+		lookup_key = (algorithm, key_id)
 
-		if expires in self._private_keys :
-			public_key = self._private_keys[expires].public_key().public_bytes(
-				encoding=serialization.Encoding.Raw,
-				format=serialization.PublicFormat.Raw,
-			)
+		if lookup_key in self._public_keyring :
+			public_key = self._public_keyring[lookup_key]
 
 		else :
 			data = self._query("""
-				SELECT public_key
-				FROM kheina.auth.token_key
-				WHERE algorithm = %s AND expires = to_timestamp(%s);
+				SELECT public_key, issued, expires
+				FROM kheina.auth.token_keys
+				WHERE algorithm = %s AND key_id = %s;
 				""",
-				(algorithm, expires),
-				fetch=True,
+				lookup_key,
+				fetch_one=True,
 			)
-			if data :
-				public_key = data[0][0]
 
-		if public_key :
-			return {
-				'algorithm': algorithm,
-				'expires': expires,
-				'public_key': bytes.decode(b64encode(public_key)),
-			}
+			if not data :
+				raise NotFound('Public key does not exist for given algorithm and key_id.')
 
-		raise InternalServerError('Public key does not exist for given expire and algorithm.')
+			public_key = bytes.decode(b64encode(data[0]))
+
+		return {
+			'algorithm': algorithm,
+			'issued': data[1],
+			'expires': data[2],
+			'public_key': public_key,
+		}
 
 
 	def close(self) :
@@ -234,7 +287,7 @@ class Authenticator :
 				WHERE email_hash = %s;
 				""",
 				(Binary(email_hash),),
-				fetch=True,
+				fetch_one=True,
 			)
 			if not data :
 				raise Unauthorized('verification failed.')
@@ -303,10 +356,10 @@ class Authenticator :
 					Binary(email_hash), Binary(password_hash), secret,
 				),
 				commit=True,
-				fetch=True,
+				fetch_one=True,
 			)
 			return {
-				'user_id': data[0][0],
+				'user_id': data[0],
 				'user': handle,
 				'name': name,
 				'token': None,
